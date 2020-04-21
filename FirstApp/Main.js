502704b1e37ba1fd14c219e4a28cccd6098dc2df
Tomislav Sever <tomislav.sever@gmx.de>
2020-04-21 17:58:43 +0200

    Final Commit after session 1

---
+++ FirstApp/Main.js
@@ -0,0 +1,7 @@
+"use strict";
+var FirstFudge;
+(function (FirstFudge) {
+    let node = new FudgeCore.Node("TestNode");
+    console.log(node);
+})(FirstFudge || (FirstFudge = {}));
+//# sourceMappingURL=Main.js.map
---
+++ FirstApp/Main.ts
@@ -0,0 +1,6 @@
+namespace FirstFudge{
+    
+    let node: FudgeCore.Node = new FudgeCore.Node("TestNode");
+
+    console.log(node);
+}
