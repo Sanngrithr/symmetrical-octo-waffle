f18eda5c580921e08f1072f26a1274b842a259a3
Tomislav Sever <tomislav.sever@gmx.de>
2020-04-27 12:58:49 +0200

    oh god node what have you done

--- FirstApp/Main.js
+++ FirstApp/Main.js
@@ -1,7 +1,26 @@
 "use strict";
 var FirstFudge;
 (function (FirstFudge) {
-    let node = new FudgeCore.Node("TestNode");
-    console.log(node);
+    var ƒ = FudgeCore;
+    window.addEventListener("load", hndLoad);
+    function hndLoad(_event) {
+        const canvas = document.querySelector("canvas");
+        ƒ.RenderManager.initialize();
+        ƒ.Debug.log(canvas);
+        let node = new ƒ.Node("Quad");
+        let mesh = new ƒ.MeshQuad();
+        let cmpMesh = new ƒ.ComponentMesh(mesh);
+        node.addComponent(cmpMesh);
+        let mtrSolidWhite = new ƒ.Material("SolidWhite", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.CSS("WHITE")));
+        let cmpMaterial = new ƒ.ComponentMaterial(mtrSolidWhite);
+        node.addComponent(cmpMaterial);
+        let cmpCamera = new ƒ.ComponentCamera();
+        cmpCamera.pivot.translateZ(2);
+        cmpCamera.pivot.rotateY(180);
+        FirstFudge.viewport = new ƒ.Viewport();
+        FirstFudge.viewport.initialize("Viewport", node, cmpCamera, canvas);
+        ƒ.Debug.log(FirstFudge.viewport);
+        FirstFudge.viewport.draw();
+    }
 })(FirstFudge || (FirstFudge = {}));
 //# sourceMappingURL=Main.js.map
