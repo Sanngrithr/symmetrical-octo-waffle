4ce6ac776503b7a3d4f0685984b1213107701e13
Tomislav Sever <tomislav.sever@gmx.de>
2020-04-28 15:51:52 +0200

    some tests and playing around

--- FirstApp/Main.js
+++ FirstApp/Main.js
@@ -1,25 +1,44 @@
 "use strict";
 var FirstFudge;
 (function (FirstFudge) {
-    var ƒ = FudgeCore;
+    var f = FudgeCore;
     window.addEventListener("load", hndLoad);
     function hndLoad(_event) {
         const canvas = document.querySelector("canvas");
-        ƒ.RenderManager.initialize();
-        ƒ.Debug.log(canvas);
-        let node = new ƒ.Node("Quad");
-        let mesh = new ƒ.MeshQuad();
-        let cmpMesh = new ƒ.ComponentMesh(mesh);
+        f.RenderManager.initialize();
+        f.Debug.log(canvas);
+        let node = new f.Node("Quad");
+        let mesh = new f.MeshQuad();
+        let cmpMesh = new f.ComponentMesh(mesh);
         node.addComponent(cmpMesh);
-        let mtrSolidWhite = new ƒ.Material("SolidWhite", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.CSS("WHITE")));
-        let cmpMaterial = new ƒ.ComponentMaterial(mtrSolidWhite);
+        let mtrSolidWhite = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(f.Color.CSS("WHITE")));
+        let cmpMaterial = new f.ComponentMaterial(mtrSolidWhite);
         node.addComponent(cmpMaterial);
-        let cmpCamera = new ƒ.ComponentCamera();
-        cmpCamera.pivot.translateZ(2);
-        cmpCamera.pivot.rotateY(180);
-        FirstFudge.viewport = new ƒ.Viewport();
+        let cmpCamera = new f.ComponentCamera();
+        cmpCamera.pivot.translateZ(5);
+        cmpCamera.pivot.translateY(1);
+        cmpCamera.pivot.translateX(-1);
+        cmpCamera.pivot.rotateY(170);
+        cmpCamera.pivot.rotateX(10);
+        //make a cube
+        let cube = new f.Node("Cube");
+        let mesh2 = new f.MeshCube();
+        let cmpMesh2 = new f.ComponentMesh(mesh2);
+        cube.addComponent(cmpMesh2);
+        cube.addComponent(cmpMaterial);
+        cube.addComponent(new f.ComponentTransform(new f.Matrix4x4()));
+        cube.mtxLocal.translateX(1);
+        node.addChild(cube);
+        //create scene lighting
+        let lightNode = new f.Node("light");
+        let light = new f.LightDirectional(new f.Color(1, 0, 0, 1));
+        let cmpLight = new f.ComponentLight(light);
+        lightNode.addComponent(cmpLight);
+        lightNode.addComponent(new f.ComponentTransform(new f.Matrix4x4()));
+        lightNode.mtxLocal.translateY(10);
+        FirstFudge.viewport = new f.Viewport();
         FirstFudge.viewport.initialize("Viewport", node, cmpCamera, canvas);
-        ƒ.Debug.log(FirstFudge.viewport);
+        f.Debug.log(FirstFudge.viewport);
         FirstFudge.viewport.draw();
     }
 })(FirstFudge || (FirstFudge = {}));
--- FirstApp/Main.ts
+++ FirstApp/Main.ts
@@ -1,31 +1,60 @@
 namespace FirstFudge {
-    import ƒ = FudgeCore;
+    import f = FudgeCore;
 
     window.addEventListener("load", hndLoad);
-    export let viewport: ƒ.Viewport;
+    export let viewport: f.Viewport;
 
     function hndLoad(_event: Event): void {
         const canvas: HTMLCanvasElement = document.querySelector("canvas");
-        ƒ.RenderManager.initialize();
-        ƒ.Debug.log(canvas);
+        f.RenderManager.initialize();
+        f.Debug.log(canvas);
 
-        let node: ƒ.Node = new ƒ.Node("Quad");
+        let node: f.Node = new f.Node("Quad");
 
-        let mesh: ƒ.MeshQuad = new ƒ.MeshQuad();
-        let cmpMesh: ƒ.ComponentMesh = new ƒ.ComponentMesh(mesh);
+        let mesh: f.MeshQuad = new f.MeshQuad();
+        let cmpMesh: f.ComponentMesh = new f.ComponentMesh(mesh);
         node.addComponent(cmpMesh);
         
-        let mtrSolidWhite: ƒ.Material = new ƒ.Material("SolidWhite", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.CSS("WHITE")));
-        let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(mtrSolidWhite);
+        let mtrSolidWhite: f.Material = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(f.Color.CSS("WHITE")));
+        let cmpMaterial: f.ComponentMaterial = new f.ComponentMaterial(mtrSolidWhite);
         node.addComponent(cmpMaterial);
 
-        let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
-        cmpCamera.pivot.translateZ(2);
-        cmpCamera.pivot.rotateY(180);
+        let cmpCamera: f.ComponentCamera = new f.ComponentCamera();
+        
+        cmpCamera.pivot.translateZ(5);
+        cmpCamera.pivot.translateY(1);
+        cmpCamera.pivot.translateX(-1);
+        cmpCamera.pivot.rotateY(170);
+        cmpCamera.pivot.rotateX(10);
 
-        viewport = new ƒ.Viewport();
+        //make a cube
+        
+        let cube: f.Node = new f.Node("Cube");
+        
+        let mesh2: f.Mesh = new f.MeshCube();
+        let cmpMesh2: f.ComponentMesh = new f.ComponentMesh(mesh2);
+        cube.addComponent(cmpMesh2);
+        cube.addComponent(cmpMaterial);
+
+        cube.addComponent(new f.ComponentTransform(new f.Matrix4x4()));
+        cube.mtxLocal.translateX(1);
+
+        node.addChild(cube);
+
+        //create scene lighting
+
+        let lightNode: f.Node = new f.Node("light");
+        let light: f.LightDirectional = new f.LightDirectional(new f.Color(1, 0 , 0, 1));
+
+        let cmpLight: f.ComponentLight = new f.ComponentLight(light);
+        lightNode.addComponent(cmpLight);
+
+        lightNode.addComponent(new f.ComponentTransform(new f.Matrix4x4()));
+        lightNode.mtxLocal.translateY(10);
+        
+        viewport = new f.Viewport();
         viewport.initialize("Viewport", node, cmpCamera, canvas);
-        ƒ.Debug.log(viewport);
+        f.Debug.log(viewport);
 
         viewport.draw();
     }
